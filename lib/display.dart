1c64566b506f81e4902d075d4d88653a582475ef
TecMochis <you@example.com>
2023-03-02 14:20:10 -0700

    comit

--- lib/display.dart
+++ lib/display.dart
@@ -1,1 +1,16 @@
 final String love='love';
+final String fifteen='fifteen';
+final String thirty='thirty';
+final String fourty='forty';
+
+class Jugador1{
+  int punto=0;
+}
+class Jugador2{
+  int punto=0;
+}
+String CanchaTenis(int a){
+  
+  if (Jugador1.punto==0 ) return love;
+  return'';
+}
