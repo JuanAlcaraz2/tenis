fc8d68d7f21a8e27448175033808511a6fbc9c2b
TecMochis <you@example.com>
2023-03-02 14:20:17 -0700

    comit2

--- test/display_test.dart
+++ test/display_test.dart
@@ -4,7 +4,11 @@ import 'package:tenis/display.dart';
 void main() {
   test('display ...', () async {
     test('Cuando inicia el partido', () {
-      
+      String punto=CanchaTenis('love');
+      expect(punto, equals(love));
     });
+  });
+  test('Primer punto', () {
+    
   });
 }
